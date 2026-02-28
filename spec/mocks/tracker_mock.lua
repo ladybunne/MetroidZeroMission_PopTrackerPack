a14cd198d3aefd9ba797a551130c72550324ef70
Ladybunne <git@ladybunne.net>
2026-02-28 20:30:18 +1100

    Do some initial work on setting up fake tracker states for testing

--- spec/mocks/tracker_mock.lua
+++ spec/mocks/tracker_mock.lua
@@ -1,15 +1,48 @@
 -- This file needs to mock out anything that would touch PopTracker's functions,
 -- so that tests can run independently of PopTracker running.
 
+-- We need a table that has all the items/locations that can be looked up.
+-- This is a bit more complex, will need to think about how to structure it.
+JSON = require("JSON")
+inspect = require("inspect")
+
+-- We also need to track current items.
 TEST_PACK_STATE = {}
+
+-- It would be useful to have some example sets of data.
+EMPTY_INVENTORY = {}
+
+local function GetFullInventory()
+    -- Probably easier to just parse the equipment file.
+    local file = io.open("items/equipment.json", "rb")
+    assert(file)
+    local content = file:read("*a")
+    file:close()
+    local table = JSON:decode(content)
+    local output = {}
+
+    for k, v in pairs(table) do
+        if v["type"] == "consumable" then
+            output[v["codes"]] = v["max_quantity"]
+        elseif v["type"] == "toggle" then
+            output[v["codes"]] = 1
+        end
+    end
+
+    return output
+end
+
+FULL_INVENTORY = GetFullInventory()
 
 ---@class Tracker
 Tracker = {}
 
+---@diagnostic disable-next-line: duplicate-set-field
 function Tracker:FindObjectForCode()
     ;
 end
 
+---@diagnostic disable-next-line: duplicate-set-field
 function Tracker:ProviderCountForCode(self, code)
     return 1
 end
